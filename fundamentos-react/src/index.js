163f813d1d993800ae7d6862022623be313f506c
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-03 13:59:32 -0300

    Primeiro teste com o React

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,8 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render (
+    <h1>Olá React!</h1>, // recebe o elemento que se quer renderizar
+    document.getElementById('root') // onde o componente vai ser renderizado
+)
