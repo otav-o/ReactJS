abafde6741913f800137c905f64e99353eeedfec
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-03 14:58:49 -0300

    Props.children

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,17 +2,19 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render (
     <div>
-        <Primeiro/>
-        <ComParametro titulo="Esse é o título"
-         subtitulo="Esse é o subtítulo"></ComParametro>
-
-         <ComParametro titulo="Oi" subtitulo="Ola"/> 
-    </div>, // duas instâncias do componente
+        <ComFilhos>
+            <ul>
+                <li>Ana</li>
+                <li>Bia</li>
+                <li>Carslos</li>
+                <li>Daniel</li>
+            </ul>
+        </ComFilhos>
+    </div>,
 
     document.getElementById('root') 
 )
