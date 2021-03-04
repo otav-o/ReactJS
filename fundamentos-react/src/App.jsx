212810d1b91a3d3de859511a85c3896b49958b98
otav-o <otaviocacadordioscanio@gmail.com>
2021-03-04 16:33:24 -0300

    Verificação de par/impar no componente Condicional

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -6,9 +6,15 @@ import Primeiro from './components/Primeiro'
 import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
 import Repeticao from './components/layout/Repeticao' // os diretórios estão todos desorganizados
+import Condicional from './components/layout/Condicional'
 
 export default (props) => (
     <div className='App'>
+
+        <Card titulo="Condicional">
+            <Condicional numero={3}></Condicional>
+        </Card>
+        {/* número é passado entre chaves no jsx */}
 
         <Card titulo="Componente de produtos">
             <Repeticao/>
